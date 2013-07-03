c8cdf235ad834bc769e5fe23e480c9d9f09cc481
Kevin Rutten <Kevin@KevinRutten.com>
2013-07-02 17:41:15 -0700

    Initial commit of Blank Rails 4.0.0 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blank400::Application.config.secret_key_base = '76fbfe28b18905f08c596f20bba0c8222fb88513cfbc64dbd206b7df64ac1f5adc25a587f0065c4ce46ab75e1141b7e82a51e96b636fd139cfcc149385cc3754'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blank400::Application.config.session_store :cookie_store, key: '_blank400_session'
